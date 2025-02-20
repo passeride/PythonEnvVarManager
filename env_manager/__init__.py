e3770666b56d3a4c64485c1462672ee3d2e929c3
Lukas <lukas@passeride.com>
2025-02-20 09:57:43 +0100

    Base

--- env_manager/__init__.py
+++ env_manager/__init__.py
@@ -1,4 +1,16 @@
 from .EnvVarManager import EnvManager
+from importlib import resources
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+# Version of the realpython-reader package
+__version__ = "1.0.0"
+
+# Read URL of the Real Python feed from config file
+_cfg = tomllib.loads(resources.read_text("reader", "config.toml"))
+URL = _cfg["feed"]["url"]
 
 
 def set_write_to_dotenv(write_to_dotenv: bool) -> None:
