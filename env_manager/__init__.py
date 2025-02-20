3bd1a40519f668067f6238fb5257d286f6040f21
Lukas <lukas@passeride.com>
2025-02-20 09:57:49 +0100

    Bump version 0.0.1 -> 0.1.0

--- env_manager/__init__.py
+++ env_manager/__init__.py
@@ -6,7 +6,7 @@ except ModuleNotFoundError:
     import tomli as tomllib
 
 # Version of the realpython-reader package
-__version__ = "1.0.0"
+__version__ = "0.1.0"
 
 # Read URL of the Real Python feed from config file
 _cfg = tomllib.loads(resources.read_text("reader", "config.toml"))
